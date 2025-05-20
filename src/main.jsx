c21488cbab074e8e5598021285aa79b843598aeb
Sergio Herrera <sehc.ing@gmail.com>
2025-05-20 10:31:51 -0400

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import Provider from './Provider.jsx'; // Asegúrate de que la ruta sea correcta
+import Provider from './Provider.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
