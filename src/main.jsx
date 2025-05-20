433bfdb86a9e87115414abfa4089f231c2092d3b
Sergio Herrera <sehc.ing@gmail.com>
2025-05-20 09:56:56 -0400

    fix netify

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import Provider from './Provider'; // Asegúrate de que la ruta sea correcta
+import Provider from './Provider.jsx'; // Asegúrate de que la ruta sea correcta
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
